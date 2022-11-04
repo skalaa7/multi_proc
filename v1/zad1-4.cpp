450dd15498c659e66d7c316aa61039ca03e54fce
skalaa7 <skalaandrej7@gmail.com>
2022-11-04 12:24:58 +0100

    MUltiprocesorsi sistemi

---
+++ v1/zad1-4.cpp
@@ -0,0 +1,40 @@
+# include <stdio.h>
+# include <string.h>
+# include <mpi.h>
+const int MAX_STRING = 100;
+using namespace std;
+int main (void){
+	int message[6];
+	int load;
+	int csize;
+	int prank;
+	MPI_Init ( NULL,NULL);
+	MPI_Comm_size(MPI_COMM_WORLD,&csize);
+	MPI_Comm_rank(MPI_COMM_WORLD,&prank);
+	/*for(int i=0;i<csize;i++)
+	{
+		if(i!=prank)
+		{
+			
+		}
+	}*/
+	for(int i=0;i<csize;i++)
+	{
+		load=10*prank+rand()%10;
+		MPI_Send(&load,1,MPI_INT,i,0,MPI_COMM_WORLD);
+	}
+	for(int i=0;i<csize;i++)
+	{
+		MPI_Resc(&load,1,MPI_INT,i,0,MPI_COMM_WORLD);
+	}
+	
+	
+	cout<<"Process "<<prank<<" recieved: ";
+	for(int i=0;i<csize;i++)
+	{
+		cout<<message[i]<<" ";
+	}
+	cout<<endl;
+	MPI_Finalize ();
+	return 0;
+}
